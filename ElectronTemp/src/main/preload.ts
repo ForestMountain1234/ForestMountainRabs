2268b610bb16ecacf20aab1a8ede059f4aac3983
ForestMountain1234 <104607216+ForestMountain1234@users.noreply.github.com>
2022-06-12 14:26:08 +0900

    comit

--- ElectronTemp/src/main/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-//■renderer公開API
-contextBridge.exposeInMainWorld('API', {
-  //公開API名: (引数) => ipcRenderer.send('ipcMainのAPI名', 引数),
-});
--- ElectronTemp/src/renderer/@types/renderer.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IElectronAPI {
-    //preload.tsで公開したAPI名を記述
-}
-
-declare global {
-    interface Window {
-        API: IElectronAPI
-    }
-}
